9f649604923d79c18bd7c311cd88652cd1a6ca9f
Vinícius Henrique Ponciano Ribeiro <viniciusponciano@dcc.ufmg.br>
2018-08-27 13:50:02 -0300

    Changed the list item to format the elements placed in.

--- imports/ui/videos/VideoItem.jsx
+++ imports/ui/videos/VideoItem.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import { ListItem, Icon } from '@material-ui/core';
+import { ListItem, Icon, Grid } from '@material-ui/core';
+import classNames from 'classnames';
 import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
+import PropTypes from 'prop-types';
+
+const style = {
+  span: { marginLeft: '5px' },
+};
 
 // VideoItemComponent - Represents the component that is a video item
 export default class VideoItemComponent extends Component {
-  constructor(props) {
+  propTypes = {
+    video: PropTypes.instanceOf(Object),
+    onClick: PropTypes.func,
+  }
+
+  constructor (props) {
     super(props);
     this.state = { progresso: {} };
     this.interval = [];
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.interval.push(Meteor.setInterval(this.obterProgresso, 3000));
   }
 
-  componentWillUpdate({ video }) {
+  componentWillUpdate ({ video }) {
     const { id } = this.props.video.meta;
     if (video.meta.id !== id) {
       this.interval.push(Meteor.setInterval(this.obterProgresso, 1000));
@@ -48,41 +59,81 @@ export default class VideoItemComponent extends Component {
     if (video.meta.id) {
       HTTP.get(url, options, callback);
     }
-  };
+  }
 
-  render() {
+  render () {
     const { video, onClick } = this.props;
-    const { progresso } = this.state;
+    const {
+      state,
+      current_event: currentEvent,
+      current_event_progress: currentEventProgress,
+      progress,
+    } = this.state.progresso;
+    const isDisable = (state !== 'finished'
+      || !(video.meta.progresso && video.meta.progresso.state === 'finished'));
+    const fontColor = isDisable ? 'action' : 'primary';
     return (
       <ListItem
         button
         key={video._id}
         id={video._id}
         onClick={() => onClick(video)}
-        disabled={progresso.state !== 'finished'
-        || !(video.meta.progresso && video.meta.progresso.state === 'finished')}
+        disabled={isDisable}
       >
-        {video.name}
-        {' '}
-        {progresso.state
-          && (
-            <span>
--
-              {progresso.current_event}
-              {' '}
--
-              {progresso.current_event_progress}
-%
-              {progresso.state}
-              {' '}
--
-              {progresso.progress}
-%
-            </span>
-          )}
-        {(progresso.state === 'finished'
-        || (video.meta.progresso && video.meta.progresso.state === 'finished'))
-        && <Icon className="fa fa-play" />}
+        <Grid container sm="auto" style={{ overflowWrap: 'break-word' }}>
+          <Grid item xs={5}>
+            {video.name}
+          </Grid>
+          <Grid item xs={6}>
+            {state
+            && (
+              <Grid container sm="auto">
+                <Grid container sm="auto">
+                  {currentEvent && (
+                    <Grid item sm="6">
+                      Evento:
+                      <span style={style.span}>
+                        {currentEvent}
+                      </span>
+                    </Grid>
+                  )}
+                  {currentEventProgress && (
+                    <Grid item sm="6">
+                      Progresso do Evento:
+                      <span style={style.span}>
+                        {currentEventProgress}
+                        %
+                      </span>
+                    </Grid>
+                  )}
+                </Grid>
+                <Grid container sm="auto">
+                  <Grid item sm="6">
+                    Situação:
+                    <span style={style.span}>
+                      {state}
+                    </span>
+                  </Grid>
+                  {progress && (
+                    <Grid item sm="6">
+                      Progresso: $
+                      <span style={style.span}>
+                        {progress}
+                        %
+                      </span>
+                    </Grid>
+                  )}
+                </Grid>
+              </Grid>
+            )}
+          </Grid>
+          <Grid item xs={1}>
+            <Icon
+              className={classNames('fa fa-play')}
+              color={fontColor}
+            />
+          </Grid>
+        </Grid>
       </ListItem>
     );
   }
--- imports/ui/videos/VideoView.jsx
+++ imports/ui/videos/VideoView.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function VideoViewComponent({ video }) {
   return (
-    <video controls>
+    <video controls onCanPlay="video/mp4">
       <source src={video.meta.url || ''} type="video/mp4" />
       <track src={video.meta.subtitle || ''} kind="subtitles" srcLang="en" label="English" />
-        Opa, o vídeo não é suportado.
+      Opa, o vídeo não é suportado.
     </video>
   );
 }
